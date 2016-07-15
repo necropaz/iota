85e47dabd0e06d6ccd2a72e9ef57fc8747fa1ff7
necropaz <jan-huber@hispeed.ch>
2016-07-15 16:21:34 +0200

    v0.1

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 
 setup(
 	name='iota',
-	version='0.1',
+	version='v0.1',
 	description='Python package to work with the IOTA API.',
 	url='https://github.com/necropaz/iota',
 	author='Necropaz',
 	author_email='jnecropaz@gmail.com',
 	license='MIT',
 	packages=['iota'],
-	download_url = 'https://github.com/necropaz/iota/tarball/0.1',
+	download_url = 'https://github.com/necropaz/iota/tarball/v0.1',
 	keywords = ['iota', 'crypto'],
 	classifiers = [],
 )
